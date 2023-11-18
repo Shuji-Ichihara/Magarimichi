c936981a2c908fe5d20ca9517ed653c02e61bde0
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-11-18 16:30:39 +0900

    Update MapManager.cs
    MapChipController を活用してプレハブを取得するようにした。

--- Magarimichi/Assets/Member/Ichihara/Scripts/MapManager.cs
+++ Magarimichi/Assets/Member/Ichihara/Scripts/MapManager.cs
@@ -6,27 +6,28 @@ public class MapManager : SingletonMonoBehaviour<MapManager>
 {
     // 配置するマップチップ
     [SerializeField]
-    private GameObject _mapChip = null;
+    private MapChipController _mapChipController = null;
     // マップの縦横比
     [SerializeField]
     private Vector2 _mapWidthAndHeight = new Vector2(4, 4);
+    // マップチップに設定するスプライトの配列
+    public List<Sprite> MapChipSprites => _mapChipSprites;
+    [SerializeField]
+    private List<Sprite> _mapChipSprites = new List<Sprite>();
 
     // マップチップ配列
-    public GameObject[,] Map => _map;
     private GameObject[,] _map = { };
 
     new private void Awake()
     {
         // null チェック
-        if (_mapChip == null)
-            _mapChip = Resources.Load("Prefabs/MapChip") as GameObject;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
+        if (_mapChipController == null)
+        {
+            GameObject obj = Resources.Load("Prefabs/MapChip") as GameObject;
+            _mapChipController = obj.GetComponent<MapChipController>();
+        }
         // 配列の大きさを設定
-        _map = new GameObject[(int)_mapWidthAndHeight.x, (int)_mapWidthAndHeight.y];
+        _map = new GameObject[(int)_mapWidthAndHeight.y, (int)_mapWidthAndHeight.x];
         GenerateMap();
     }
 
@@ -36,29 +37,43 @@ public class MapManager : SingletonMonoBehaviour<MapManager>
     private void GenerateMap()
     {
         int half = 2;
+        Vector2 hiddenMapChip = new Vector2(3, 0);
         // _map[0,0] の座標
         Vector3 mapChipPosition = new Vector3(
-            -(_mapChip.transform.localScale.x * _mapWidthAndHeight.x / half) + _mapChip.transform.localScale.x / half
-           , (_mapChip.transform.localScale.y * _mapWidthAndHeight.y / half) - _mapChip.transform.localScale.y / half);
-        for (int i = 0; i < _mapWidthAndHeight.y; i++)
+            -(_mapChipController.transform.localScale.x * _mapWidthAndHeight.x / half) + _mapChipController.transform.localScale.x / half
+           , (_mapChipController.transform.localScale.y * _mapWidthAndHeight.y / half) - _mapChipController.transform.localScale.y / half);
+        for (int heightCOunt = 0; heightCOunt < _mapWidthAndHeight.y; heightCOunt++)
         {
             // 一列配置する度に、次に配置するマップチップを 1 個分ずつ下にずらす
-            if (i != 0)
+            if (heightCOunt != 0)
             {
-                var dummyObj = _map[0, i - 1];
+                var dummyObj = _map[0, heightCOunt - 1];
                 mapChipPosition = new Vector3(
                     dummyObj.transform.position.x
-                  , dummyObj.transform.position.y - _mapChip.transform.localScale.y);
+                  , dummyObj.transform.position.y - _mapChipController.transform.localScale.y);
             }
-            for (int j = 0; j < _mapWidthAndHeight.x; j++)
+            for (int widthCount = 0; widthCount < _mapWidthAndHeight.x; widthCount++)
             {
                 // 配置する度にマップチップを 1 個分ずつ右にずらす
-                if (j != 0)
-                    mapChipPosition += Vector3.right * _mapChip.transform.localScale.x;
-                GameObject mapChipObj = Instantiate(_mapChip, mapChipPosition, Quaternion.identity);
+                if (widthCount != 0)
+                    mapChipPosition += Vector3.right * _mapChipController.transform.localScale.x;
+                GameObject mapChipObj = Instantiate(_mapChipController.gameObject, mapChipPosition, Quaternion.identity);
                 mapChipObj.transform.SetParent(transform);
-                _map[j, i] = mapChipObj;
+                _map[widthCount, heightCOunt] = mapChipObj;
             }
         }
+        //_mapChipController.SetMapChipSprite();
+        SpriteRenderer renderer = _map[(int)hiddenMapChip.y, (int)hiddenMapChip.x].GetComponent<SpriteRenderer>();
+        renderer.enabled = false;
+    }
+
+    public GameObject GetMapChipData(int height, int width)
+    {
+        if (height >= _mapWidthAndHeight.y || width >= _mapWidthAndHeight.x)
+        {
+            Debug.LogError($"{_map[height, width]}の情報が取得できません。");
+            return null;
+        }
+        return _map[height, width];
     }
 }
