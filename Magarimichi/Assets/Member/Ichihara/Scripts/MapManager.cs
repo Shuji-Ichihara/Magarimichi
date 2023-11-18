13ab1071a845591c68f9878592f90a41080e5988
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-11-18 17:53:15 +0900

    Update MapManager.cs
    鍵と錠前を生成する機能を実装した。

--- Magarimichi/Assets/Member/Ichihara/Scripts/MapManager.cs
+++ Magarimichi/Assets/Member/Ichihara/Scripts/MapManager.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class MapManager : SingletonMonoBehaviour<MapManager>
 {
+    #region Refarences
     // 配置するマップチップ
     [SerializeField]
     private MapChipController _mapChipController = null;
-    // マップの縦横比
     [SerializeField]
-    private Vector2 _mapWidthAndHeight = new Vector2(4, 4);
+    private GameObject _key = null;
+    [SerializeField]
+    private GameObject _lock = null;
     // マップチップに設定するスプライトの配列
     public List<Sprite> MapChipSprites => _mapChipSprites;
     [SerializeField]
     private List<Sprite> _mapChipSprites = new List<Sprite>();
+    #endregion
+    #region マップの大きさ
+    // マップの縦横比
+    [SerializeField]
+    private Vector2 _mapWidthAndHeight = new Vector2(4, 4);
+    #endregion
 
     // マップチップ配列
+    // 配列のイメージは、[縦のマップチップ, 横のマップチップ]でお願いします。
     private GameObject[,] _map = { };
 
     new private void Awake()
@@ -26,9 +35,20 @@ public class MapManager : SingletonMonoBehaviour<MapManager>
             GameObject obj = Resources.Load("Prefabs/MapChip") as GameObject;
             _mapChipController = obj.GetComponent<MapChipController>();
         }
+        if (_key == null)
+        {
+            GameObject obj = Resources.Load("Prefabs/Key") as GameObject;
+            _key = obj;
+        }
+        if (_lock == null)
+        {
+            GameObject obj = Resources.Load("Prefabs/Lock") as GameObject;
+            _lock = obj;
+        }
         // 配列の大きさを設定
         _map = new GameObject[(int)_mapWidthAndHeight.y, (int)_mapWidthAndHeight.x];
         GenerateMap();
+        GenerateObject();
     }
 
     /// <summary>
@@ -43,24 +63,24 @@ public class MapManager : SingletonMonoBehaviour<MapManager>
         Vector3 mapChipPosition = new Vector3(
             -(_mapChipController.transform.localScale.x * _mapWidthAndHeight.x / half) + _mapChipController.transform.localScale.x / half
            , (_mapChipController.transform.localScale.y * _mapWidthAndHeight.y / half) - _mapChipController.transform.localScale.y / half);
-        for (int heightCOunt = 0; heightCOunt < _mapWidthAndHeight.y; heightCOunt++)
+        for (int heightMapChipCount = 0; heightMapChipCount < _mapWidthAndHeight.y; heightMapChipCount++)
         {
             // 一列配置する度に、次に配置するマップチップを 1 個分ずつ下にずらす
-            if (heightCOunt != 0)
+            if (heightMapChipCount != 0)
             {
-                var dummyObj = _map[heightCOunt - 1, 0];
+                var dummyObj = _map[heightMapChipCount - 1, 0];
                 mapChipPosition = new Vector3(
                     dummyObj.transform.position.x
                   , dummyObj.transform.position.y - _mapChipController.transform.localScale.y);
             }
-            for (int widthCount = 0; widthCount < _mapWidthAndHeight.x; widthCount++)
+            for (int widthMapChipCount = 0; widthMapChipCount < _mapWidthAndHeight.x; widthMapChipCount++)
             {
                 // 配置する度にマップチップを 1 個分ずつ右にずらす
-                if (widthCount != 0)
+                if (widthMapChipCount != 0)
                     mapChipPosition += Vector3.right * _mapChipController.transform.localScale.x;
                 GameObject mapChipObj = Instantiate(_mapChipController.gameObject, mapChipPosition, Quaternion.identity);
                 mapChipObj.transform.SetParent(transform);
-                _map[heightCOunt, widthCount] = mapChipObj;
+                _map[heightMapChipCount, widthMapChipCount] = mapChipObj;
             }
         }
         //_mapChipController.SetMapChipSprite();
@@ -68,6 +88,23 @@ public class MapManager : SingletonMonoBehaviour<MapManager>
         renderer.enabled = false;
     }
 
+    /// <summary>
+    /// マップ内に存在するオブジェクトを生成
+    /// </summary>
+    private void GenerateObject()
+    {
+        Vector2 keySpawnPosition = new Vector2(2, 1);
+        Vector2 lockSpawnPosition = new Vector2(4, 3);
+        _key = Instantiate(_key, _map[(int)keySpawnPosition.y, (int)keySpawnPosition.x].transform.position, Quaternion.identity);
+        _lock = Instantiate(_lock, _map[(int)lockSpawnPosition.y, (int)lockSpawnPosition.x].transform.position, Quaternion.identity);
+    }
+
+    /// <summary>
+    /// 配列の中のマップチップの情報を取得
+    /// </summary>
+    /// <param name="height">マップの列</param>
+    /// <param name="width">マップの行</param>
+    /// <returns>マップチップの情報</returns>
     public GameObject GetMapChipData(int height, int width)
     {
         if (height >= _mapWidthAndHeight.y || width >= _mapWidthAndHeight.x)
