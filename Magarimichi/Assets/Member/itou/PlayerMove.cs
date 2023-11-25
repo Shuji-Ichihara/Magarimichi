e50a6306c3663b9ad014d170a4928e33d17247f8
a <>
2023-11-25 18:00:58 +0900

    hgff

--- Magarimichi/Assets/Member/itou/PlayerMove.cs
+++ Magarimichi/Assets/Member/itou/PlayerMove.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private float _moveTime;
-    [SerializeField] private float _moveMaxTime;
+    [SerializeField] private int _movespeed;
     [SerializeField] private Image _Keyimage;
+    [SerializeField] private GameObject playermoves;
+    private Transform _movetransfrom;
     private float colorrock = 1.0f;
     public float MoveSpeed = 3.0f;
     private int direction = 1;
@@ -19,18 +20,48 @@ public class PlayerMove : MonoBehaviour
     void Start()
     {
         _Keyimage.color = new Color(colorrock, colorrock, colorrock, 0.3f);
+        _movetransfrom = playermoves.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_moveTime >= _moveMaxTime)
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _Up = true;
+            _movetransfrom.position = new Vector3(this.transform.position.x, this.transform.position.y + _movespeed, this.transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _Right = true;
+            _movetransfrom.position = new Vector3(this.transform.position.x + _movespeed, this.transform.position.y, this.transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _Down = true;
+            _movetransfrom.position = new Vector3(this.transform.position.x, this.transform.position.y - _movespeed, this.transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _Left = true;
+            _movetransfrom.position = new Vector3(this.transform.position.x - _movespeed, this.transform.position.y, this.transform.position.z);
+        }
+        if (this.transform.position.y >= _movetransfrom.position.y)
         {
             _Up = false ;
-            _Down = false ;
-            _Left = false ;
-            _Right = false ;
-            _moveTime = 0 ;
+        }
+        if (this.transform.position.x >= _movetransfrom.position.x)
+        {
+            _Right = false;
+        }
+        if (this.transform.position.y <= _movetransfrom.position.y)
+        {
+            _Down = false;
+        }
+        if (this.transform.position.x <= _movetransfrom.position.x)
+        {
+            _Left = false;
         }
         if (_Up)
         {
@@ -47,23 +78,6 @@ public class PlayerMove : MonoBehaviour
         else if (_Left)
         {
             this.transform.position = new Vector3(this.transform.position.x - MoveSpeed * Time.deltaTime, this.transform.position.y, this.transform.position.z);
-        }
-
-        if(Input.GetKeyDown(KeyCode.W)) 
-        { 
-            _Up = true;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            _Right = true;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            _Down = true;
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            _Left = true;
         }
     }
 
