4d3f56f97cb5f1e76fb0dbb85ae70eb43f769569
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-12-09 08:52:01 +0900

    Update TitleController.cs
    BGM 再生を実装した。

--- Magarimichi/Assets/Member/Masato/Scripts/TitleController.cs
+++ Magarimichi/Assets/Member/Masato/Scripts/TitleController.cs
@@ -8,13 +8,6 @@ public class TitleController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-
+        SoundManager.instance.PlayBGM(SoundManager.E_BGM.OutGameBGM);
     }
 }
