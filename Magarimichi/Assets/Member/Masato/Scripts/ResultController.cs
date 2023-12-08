739d2ec88e45905d5e144d83d92470d5b8f08a62
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-12-09 08:51:49 +0900

    Update ResultController.cs
    BGM 再生を実装した。

--- Magarimichi/Assets/Member/Masato/Scripts/ResultController.cs
+++ Magarimichi/Assets/Member/Masato/Scripts/ResultController.cs
@@ -7,12 +7,7 @@ public class ResultController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SoundManager.instance.BGMPause();
+        SoundManager.instance.PlayBGM(SoundManager.E_BGM.OutGameBGM);
     }
 }
